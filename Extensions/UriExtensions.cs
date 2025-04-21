54440ecbfd04ccd8a2777cc2a753b20ddfeebbbd
Marcin Gutka <marcin.gutka@gmail.com>
2025-04-21 22:11:33 +0200

    Covered UriExtensions

--- Extensions/UriExtensions.cs
+++ Extensions/UriExtensions.cs
@@ -1,13 +1,20 @@
 ﻿namespace MBatch.Azure.Extensions
 {
+    /// <summary>
+    /// Static class for extensions methods for <see cref="Uri"/>.
+    /// </summary>
     public static class UriExtensions
     {
+        /// <summary>
+        /// Decomposes Blob storage Uri into container name and file path.
+        /// </summary>
+        /// <param name="uri">Blob storage Uri.</param>
         public static (string ContainerName, string FullFilePath) GetBlobAbsolutePathParts(this Uri uri)
         {
             var absolutePath = uri.AbsolutePath;
 
             if (string.IsNullOrWhiteSpace(absolutePath))
-                throw new ArgumentException("Provided Uri by batch service does not contain absolute path.");
+                throw new ArgumentException("Provided Uri does not contain absolute path.");
 
             var indexOfLastSlash = absolutePath.LastIndexOf('/');
 
